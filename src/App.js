1b7a4c9acfd41c81abdae1db9d50fc506f1027e8
Edison Un <chonwaiun@gmail.com>
2019-09-17 10:53:22 +0800

    Add the TailwindCSS into the project.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "./index.css";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
+                <p className="text-red-600">
                     Edit
-                    <code>src/App.js</code>
+                    <code> src/App.js </code>
                     and save to reload.
                 </p>
                 <a
